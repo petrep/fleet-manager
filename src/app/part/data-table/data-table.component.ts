0a39c3ea414fbc2f7228d73cc36a89b7b262c403
petrep <peter.petrekanics@gmail.com>
2022-11-13 14:52:39 +0100

    Code works ok - will start lesson: 2 way data binding

--- src/app/part/data-table/data-table.component.ts
+++ src/app/part/data-table/data-table.component.ts
@@ -6,10 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./data-table.component.css']
 })
 export class DataTableComponent implements OnInit {
+  cols: {key:string, text: string}[] = [
+    {key: 'id', text: '#'},
+    {key: 'name', text: 'Name'},
+    {key: 'email', text: 'Email'},
+    {key: 'phone', text: 'Phone'},
+    {key: 'city', text: 'City'},
+    {key: 'address', text: 'Street Address'}
+  ];
 
-  constructor() { }
+  deleteIconClass: string = 'fa fa-trash-o';
+
+  constructor() {
+    setInterval( () => {
+      if (this.deleteIconClass == 'fa fa-trash-o') {
+        this.deleteIconClass = 'fa fa-trash';
+      } else {
+        this.deleteIconClass = 'fa fa-trash-o';
+      }
+    }, 1000);
+   }
 
   ngOnInit(): void {
+  }
+
+  onUpdate($event): void {
+    alert('update');
+  }
+
+  onDelete($event): void {
+    alert(`Clicked on ${$event} button`);
   }
 
 }
