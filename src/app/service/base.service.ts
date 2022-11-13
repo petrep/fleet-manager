8dd2acae7993a0d91e5c4f63a82e2ad85bf8314c
petrep <peter.petrekanics@gmail.com>
2022-11-13 17:14:28 +0100

    Code works ok - will start lesson 4/4: latod h nem latod

--- src/app/service/base.service.ts
+++ src/app/service/base.service.ts
@@ -8,14 +8,30 @@ export class BaseService {
   data = {
     drivers: [
       {
-      id: 1,
-      name: "John Doe",
-      email: "jd@gmail.com",
-      phone: "+3612131231",
-      city: "Los Angeles",
-      address: "My address"
+        id: 1,
+        name: "John Doe",
+        email: "jd@gmail.com",
+        phone: "+3612131231",
+        city: "Los Angeles",
+        address: "My address"
+      },
+      {
+        id: 2,
+        name: "Jack Idol",
+        email: "idol@gmail.com",
+        phone: "+36123424321",
+        city: "Boston",
+        address: "My address"
+      },
+      {
+        id: 3,
+        name: "Charlie Filpo",
+        email: "filpo@gmail.com",
+        phone: "+36288424321",
+        city: "Chicago",
+        address: "My address"
       }
-    ]    
+    ]
 
   };
 
